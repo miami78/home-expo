2a893b64a8662238f0129890f7639f499d3f8895
miami78 <miamilarry78@gmail.com>
2019-08-15 13:09:51 +0400

    added a constructor

--- index.js
+++ index.js
@@ -1,52 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Navigation from "./components/nav/Navigation"
 import InfoPopup from "./components/popup/InfoPopup"
+import Card from "./components/card/Card"
 
 export {
   Navigation,
-  InfoPopup
+  InfoPopup,
+  Card,
 };
 
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
 } from 'react-360';
 
 export default class home_expo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
   render() {
     return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Home expo 1.0
-          </Text>
-        </View>
-      </View>
+        
     );
   }
 };
-
-const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
-});
 
 AppRegistry.registerComponent('home_expo', () => home_expo);
