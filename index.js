e4ab20dc447daa48b4133a900a1ffbeace603d30
miami78 <miamilarry78@gmail.com>
2019-08-20 16:22:47 +0400

    added click events

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import Navigation from "./components/nav/Navigation"
-import InfoPopup from "./components/popup/InfoPopup"
-import Card from "./components/card/Card"
+import { AppRegistry, StyleSheet, Text, View, VrButton } from 'react-360';
 
-export {
-  Navigation,
-  InfoPopup,
-  Card,
-};
-
-import {
-  AppRegistry,
-} from 'react-360';
 
 export default class home_expo extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-
+    sceneId: null,
+    nextSceneId: null,
     };
+  }
+  onClick(toId){
+    let scene = this.nextSceneId( toId );
+    this.setScene( scene );
   }
   render() {
     return (
-        
+      <View style={styles.scene}>
+        <VrButton onClick={this.props.onClick}>
+          <Text style={styles.message}>Next Scene{this.state.nextSceneId }</Text>
+        </VrButton>
+      </View>
     );
   }
 };
-
+const styles = StyleSheet.create({
+  scene : {
+    padding : 20,
+    backgroundColor : '#000000',
+    borderColor : '#639dda',
+    borderWidth : 2
+  },
+  message: {
+    fontSize : 30
+  }
+})
 AppRegistry.registerComponent('home_expo', () => home_expo);
